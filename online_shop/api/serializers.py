bf83c3c1a25eada54a8479fb6269e6b2b1a9fcc5
ulan-sharinbekov <73186262+ulan-sharinbekov@users.noreply.github.com>
2022-06-17 16:39:05 +0600

    Update serializers.py

--- online_shop/api/serializers.py
+++ online_shop/api/serializers.py
@@ -1,1 +1,45 @@
 
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from .models import *
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ProductCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+
+class ProductShortSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'title', 'price', 'articule', 'currency', 'count', 'description', 'ratingg')
+
+
+class ProductSerializer(ProductShortSerializer):
+    category = CategorySerializer(read_only=True)
+    class Meta(ProductShortSerializer.Meta):
+        fields = ProductShortSerializer.Meta.fields + ('category',)
+
+# class ProductRatingSerializer(ProductShortSerializer):
+#     class Meta(ProductShortSerializer.Meta):
+#         fields = ProductShortSerializer.Meta.fields + ('ratingg', )
+
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = '__all__'
